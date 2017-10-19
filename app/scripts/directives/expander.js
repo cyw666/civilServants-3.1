784f46cd11a89fbc5cf61dbd074b9e3f130a907e
CYW <13656631764@163.com>
2017-10-19 11:28:10 +0800

    first-commit

---
+++ app/scripts/directives/expander.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name luZhouApp.directive:expander
+ * @description
+ * # expander
+ */
+angular.module('luZhouApp')
+  .directive('expander', function () {
+    return {
+      templateUrl: 'components/expander.html',
+      restrict: 'EA',
+      transclude: true,
+      scope: {
+        title: '=expanderTitle',
+        titleClick: '=titleClick',
+        channelId: '=channelId',
+        name: '=name',
+        ptitle:'=ptitle'
+      },
+      link: function (scope, element, attrs) {
+        scope.showMe = false;
+        if (scope.name === 'course') {
+          scope.params = {channelId:scope.channelId,title: '',sort: 'Sort',order: 'desc',courseType: 'All',teacher: '',page: 1}
+        }else if(scope.name === 'book'){
+          scope.params ={categoryId:scope.channelId,ptitle:scope.ptitle,title: '',page: 1}
+        }
+        scope.toggle = function toggle() {
+          scope.showMe = !scope.showMe;
+        }
+      }
+    };
+  });
